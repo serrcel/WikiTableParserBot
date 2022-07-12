42d1f5faed04123b3b62c047398bf76ed3f61b59
Соловьев Роман <serrcel@gmail.com>
2022-07-12 10:19:50 +0400

    Echo bot

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import logging
 from config import TOKEN
 from aiogram import Bot, Dispatcher, executor, types
 
-
 API_TOKEN = TOKEN
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Привет, я бот-парсер и я помогу получить список городов!")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
